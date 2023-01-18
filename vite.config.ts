6d75d0f41eec81888ade63e8828bea6fe9f8828e
Antony1060 <antoniostignjedec@gmail.com>
2023-01-19 00:03:16 +0100

    Remove unused monaco-editor

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import monacoEditorPlugin from "vite-plugin-monaco-editor";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,9 +9,6 @@ export default defineConfig({
                 plugins: ["babel-plugin-twin", "babel-plugin-macros", "babel-plugin-styled-components"],
                 ignore: ["\x00commonjsHelpers.js"] // weird babel-macro bug workaround
             }
-        }),
-        monacoEditorPlugin({
-            languageWorkers: ["css", "html", "json", "typescript", "editorWorkerService"]
         })
     ],
     server: {
