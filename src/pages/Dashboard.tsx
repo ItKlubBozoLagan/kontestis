50ddfbf402598cb45e1e55fe1cdc3bf96208a39c
VisenP <visen.pavlica12@gmail.com>
2023-01-20 02:49:11 +0100

    Fix dashboard design

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,12 +1,19 @@
 import { useEffect, useState } from "react";
 import { FC } from "react";
+
 import { http, wrapAxios } from "../api/http";
+import { Header } from "../components/Header";
+import {
+    Table,
+    TableHeadItem,
+    TableHeadRow,
+    TableItem,
+    TableRow,
+} from "../components/Table";
+import { useAuthStore } from "../state/auth";
 import { ContestType } from "../types/ContestType";
 import { ProblemType } from "../types/ProblemType";
 import { SubmissionType } from "../types/SubmissionType";
-import { useAuthStore } from "../state/auth";
-import { Header } from "../components/Header";
-
 
 export const Dashboard: FC = () => {
     const { user } = useAuthStore();
@@ -16,44 +23,38 @@ export const Dashboard: FC = () => {
     const [totalSubmissions, setTotalSubmissions] = useState<number>(0);
 
     useEffect(() => {
-        wrapAxios<ContestType[]>(http.get("/contests")).then(
-            (c) => {
-                setTotalCOntests(c.length);
-            }
-        );
+        wrapAxios<ContestType[]>(http.get("/contests")).then((c) => {
+            setTotalCOntests(c.length);
+        });
 
-        wrapAxios<ProblemType[]>(http.get("/problems")).then(
-            (p) => {
-                setTotalProblems(p.length);
-            }
-        );
+        wrapAxios<ProblemType[]>(http.get("/problems")).then((p) => {
+            setTotalProblems(p.length);
+        });
 
-        wrapAxios<[SubmissionType[]]>(http.get("/submission", {
-            params: {user_id: user.id}
-        })).then(
-            (s) => {
-                setTotalSubmissions(s.length);
-            }
-        );
+        wrapAxios<[SubmissionType[]]>(
+            http.get("/submission", {
+                params: { user_id: user.id },
+            })
+        ).then((s) => {
+            setTotalSubmissions(s.length);
+        });
+    }, [user]);
 
-    }, [user])
-    
-
-    return <div>
-        <Header/>
-        <div tw="border-solid border-black border-2 rounded-md flex flex-col justify-items-center">
-            <div tw="bg-neutral-400 rounded-md px-2 py-2.5 flex flex-row justify-between justify-items-center">
-                <span>Total contests</span>
-                <span tw="">{totalContests}</span>
-            </div>
-            <div tw="bg-neutral-400 rounded-md px-2 py-2.5 flex flex-row justify-between justify-items-center">
-                <span>Total problems</span>
-                <span>{totalProblems}</span>
-            </div>
-            <div tw="bg-neutral-400 rounded-md px-2 py-2.5 flex flex-row justify-between justify-items-center">
-                <span>Total submissions</span>
-                <span>{totalSubmissions}</span>
-            </div>
+    return (
+        <div>
+            <Header />
+            <Table tw={"w-full"}>
+                <TableHeadRow>
+                    <TableHeadItem>Total Contests</TableHeadItem>
+                    <TableHeadItem>Total Problems</TableHeadItem>
+                    <TableHeadItem>Total Submissions</TableHeadItem>
+                </TableHeadRow>
+                <TableRow>
+                    <TableItem>{totalContests}</TableItem>
+                    <TableItem>{totalProblems}</TableItem>
+                    <TableItem>{totalSubmissions}</TableItem>
+                </TableRow>
+            </Table>
         </div>
-    </div>;
+    );
 };
