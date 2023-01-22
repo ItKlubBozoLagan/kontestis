163a5fabf8693008b11bc6bbe02aee7574aee1ad
Antony1060 <antoniostignjedec@gmail.com>
2023-01-22 14:09:20 +0100

    Introduce .env files

--- src/api/http.ts
+++ src/api/http.ts
@@ -3,8 +3,11 @@ import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 import { useAuthStore } from "../state/auth";
 
 export const http = axios.create({
-    baseURL: "https://kontestis.antony.cash/api",
+    baseURL:
+        (import.meta.env.VITE_API_ENDPOINT ?? "http://localhost:8080") + "/api",
 });
+
+console.log(http.defaults.baseURL);
 
 http.interceptors.request.use((config: AxiosRequestConfig) => {
     const { token } = useAuthStore.getState();
