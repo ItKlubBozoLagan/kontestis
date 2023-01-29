776505ca746c110e0fb7b1d37b15d04201f9cac6
VisenP <visen.pavlica12@gmail.com>
2023-01-29 21:34:43 +0100

    Improve account page design

--- apps/frontend/src/pages/account/Account.tsx
+++ apps/frontend/src/pages/account/Account.tsx
@@ -1,19 +1,41 @@
+import md5 from "md5";
 import { FC } from "react";
 
+import { SimpleButton } from "../../components/SimpleButton";
+import { TitledInput } from "../../components/TitledInput";
+import { TitledSection } from "../../components/TitledSection";
 import { useAuthStore } from "../../state/auth";
-import { capitalize } from "../../utils/text";
 
 export const Account: FC = () => {
     const { user } = useAuthStore();
 
     return (
-        <div tw={"flex flex-col gap-2 py-10"}>
-            {Object.entries(user).map(([field, value]) => (
-                <div key={field} tw={"flex gap-2 text-lg"}>
-                    <span tw={"font-bold"}>{capitalize(field)}: </span>
-                    <span tw={"font-mono"}>{value.toString()}</span>
+        <div tw={"w-full md:w-4/5 flex flex-col gap-2 py-10"}>
+            <TitledSection title={"Account"}>
+                <div tw={"w-full flex items-center justify-between px-10"}>
+                    <div tw={"flex flex-col justify-start gap-2 font-mono"}>
+                        <img
+                            tw={"w-64 h-64 rounded-full"}
+                            src={`https://www.gravatar.com/avatar/${md5(
+                                user.email.trim().toLowerCase()
+                            )}`}
+                            alt={"Profile avatar"}
+                        />
+                        <span tw={"text-[10px]"}>
+                            Image provided by{" "}
+                            <a href={"https://en.gravatar.com/"}>Gravatar</a>!
+                        </span>
+                    </div>
+                    <div tw={"flex flex-col justify-between gap-y-2 font-mono"}>
+                        <TitledInput title={"Name:"} value={user.username} />
+                        <TitledInput title={"Email:"} value={user.email} />
+                        <TitledInput title={"New Password:"} value={""} />
+                        <TitledInput title={"Confirm Password:"} value={""} />
+                        <TitledInput title={"Current Password:"} value={""} />
+                        <SimpleButton>Update details!</SimpleButton>
+                    </div>
                 </div>
-            ))}
+            </TitledSection>
         </div>
     );
 };
