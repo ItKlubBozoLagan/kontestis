fac1e4a8902b7daf6acc00727b8b65df7c7d3a86
VisenP <visen.pavlica@skole.hr>
2023-03-21 14:32:16 +0100

    Sort contests

--- apps/frontend/src/pages/contests/ContestsTable.tsx
+++ apps/frontend/src/pages/contests/ContestsTable.tsx
@@ -1,4 +1,4 @@
-import { ContestWithPermissions } from "@kontestis/models";
+import { Contest, ContestWithPermissions } from "@kontestis/models";
 import { FC } from "react";
 
 import { Table, TableHeadItem, TableHeadRow } from "../../components/Table";
@@ -7,6 +7,13 @@ import { ContestListItem } from "./ContestListItem";
 type Properties = {
     contests: ContestWithPermissions[];
 };
+
+const hasStarted: (contest: Contest) => boolean = (contest) =>
+    Date.now() > contest.start_time.getTime() &&
+    Date.now() < contest.start_time.getTime() + contest.duration_seconds * 1000;
+
+const hasEnded: (contest: Contest) => boolean = (contest) =>
+    Date.now() > contest.start_time.getTime() + contest.duration_seconds * 1000;
 
 export const ContestsTable: FC<Properties> = ({ contests }) => {
     return (
@@ -21,9 +28,19 @@ export const ContestsTable: FC<Properties> = ({ contests }) => {
                 </TableHeadRow>
             </thead>
             <tbody>
-                {contests.map((c) => (
-                    <ContestListItem contest={c} key={c.id.toString()} />
-                ))}
+                {contests
+                    .sort((a, b) => {
+                        if (hasStarted(a) != hasStarted(b)) return hasStarted(a) ? -1 : 1;
+
+                        if (hasEnded(a) != hasEnded(b)) return hasEnded(a) ? 1 : -1;
+
+                        if (hasEnded(a)) return b.start_time.getTime() - a.start_time.getTime();
+
+                        return a.start_time.getTime() - b.start_time.getTime();
+                    })
+                    .map((c) => (
+                        <ContestListItem contest={c} key={c.id.toString()} />
+                    ))}
             </tbody>
         </Table>
     );
