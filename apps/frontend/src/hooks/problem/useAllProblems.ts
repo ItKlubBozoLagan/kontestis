6459a8f0b7e55e5fded512d0295a7e00cf280f22
VisenP <visen.pavlica12@gmail.com>
2023-03-25 17:01:11 +0100

    Fix problem query key

--- apps/frontend/src/hooks/problem/useAllProblems.ts
+++ apps/frontend/src/hooks/problem/useAllProblems.ts
@@ -9,7 +9,7 @@ export const useAllProblems: QueryHandler<ProblemWithScore[], Snowflake | undefi
     options
 ) =>
     useQuery({
-        queryKey: contestId ? ["contests", contestId, "problem"] : ["problem"],
+        queryKey: contestId ? ["contests", contestId, "problems"] : ["problem"],
         queryFn: () =>
             wrapAxios(
                 http.get("/problem", contestId ? { params: { contest_id: contestId } } : undefined)
