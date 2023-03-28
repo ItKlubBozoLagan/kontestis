13f0c809598540e3bd5941b68da11f87565603e3
Antony1060 <antoniostignjedec@gmail.com>
2023-03-28 11:12:29 +0200

    Fix lint

--- apps/backend/src/lib/elo.ts
+++ apps/backend/src/lib/elo.ts
@@ -6,26 +6,20 @@ export type ContestMemberLeaderboardInfo = {
     problemPoints: number[];
 };
 
-const computePlace = (
-    userRatings: number[],
-    score: number,
-) => {
+const computePlace = (userRatings: number[], score: number) => {
+    return userRatings.reduce((ep = 1, mem) => ep + 1 / (1 + 10 ** ((score - mem) / 400)));
+};
 
-    const expectedPlace = userRatings.reduce((ep = 1, mem) => ep + 1/(1+10**((score-mem)/400)));
-    return expectedPlace;
-}
+const computePerformance = (userRatings: number[], place: number) => {
+    let low = 0;
+    let high = 10_000;
 
-const computePerformance = (
-    userRatings: number[],
-    place: number,
-) => {
-    var low = 0;
-    var high = 10000;
-    while (high-low>0.5){
-        const mid = low+(high-low)/2;
-        if (computePlace(userRatings, mid) < place){
+    while (high - low > 0.5) {
+        const mid = low + (high - low) / 2;
+
+        if (computePlace(userRatings, mid) < place) {
             low = mid;
-        }else{
+        } else {
             high = mid;
         }
     }
@@ -46,15 +40,21 @@ export const computeELODifference = (
 
     const score = targetMember.problemPoints.reduce((sum, p) => sum + p, 0);
 
-    const expectedPlace = userRatings.reduce((ep, mem) => ep + 1/(1+10**((score-mem)/400)), 0);
+    const expectedPlace = userRatings.reduce(
+        (ep, mem) => ep + 1 / (1 + 10 ** ((score - mem) / 400)),
+        0
+    );
 
-    const place = userRatings.reduce((p, mem) => p + (mem > score ? 1 : 0) + (mem === score ? 0.5 : 0), 0);
+    const place = userRatings.reduce(
+        (p, mem) => p + (mem > score ? 1 : 0) + (mem === score ? 0.5 : 0),
+        0
+    );
 
-    const newPlace = Math.sqrt(expectedPlace*place);
+    const newPlace = Math.sqrt(expectedPlace * place);
 
     const performanceRating = computePerformance(userScores, newPlace);
 
-    const result = Math.round((targetMember.currentGlobalElo-performanceRating)/2)
+    const result = Math.round((targetMember.currentGlobalElo - performanceRating) / 2);
 
     return Number.isNaN(result) ? 0 : result;
 };
