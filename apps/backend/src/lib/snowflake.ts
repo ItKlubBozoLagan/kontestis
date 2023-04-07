d8c5eb41118113abb630dbe598a09952275b06a0
Antony1060 <antoniostignjedec@gmail.com>
2023-04-07 23:37:49 +0200

    Bump typebox

--- apps/backend/src/lib/snowflake.ts
+++ apps/backend/src/lib/snowflake.ts
@@ -1,17 +1,17 @@
 import { Snowflake } from "@kontestis/models";
 import { decode, generateSunflake, SunflakeConfig } from "sunflake";
 
-const sunFlakeConfig: SunflakeConfig<"bigint"> = {
+const sunflakeConfig: SunflakeConfig<"bigint"> = {
     machineId: process.env.MACHINE_ID ?? 1,
     as: "bigint",
 };
 
 export const generateSnowflake: () => Snowflake = (() => {
-    const sunflake = generateSunflake(sunFlakeConfig);
+    const sunflake = generateSunflake(sunflakeConfig);
 
     return () => sunflake();
 })();
 
 export const getSnowflakeTime: (s: Snowflake) => Date = (snowflake: Snowflake) => {
-    return new Date(Number(decode(snowflake, sunFlakeConfig).time));
+    return new Date(Number(decode(snowflake, sunflakeConfig).time));
 };
