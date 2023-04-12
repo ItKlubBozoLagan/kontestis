2feb9c95663f71cdc12fea09e6d07d31fb35117f
Antony1060 <antoniostignjedec@gmail.com>
2023-04-12 18:43:40 +0200

    Fix wrong problem extractor typing

--- apps/backend/src/extractors/extractProblem.ts
+++ apps/backend/src/extractors/extractProblem.ts
@@ -3,6 +3,7 @@ import {
     ContestMemberPermissions,
     hasAdminPermission,
     hasContestPermission,
+    Problem,
     Snowflake,
 } from "@kontestis/models";
 import { Request } from "express";
@@ -22,45 +23,68 @@ export const extractProblem = (
     req: Request,
     problemId: Snowflake = extractIdFromParameters(req, "problem_id")
 ) =>
-    memoizedRequestExtractor(req, "__problem_" + problemId, async () => {
-        const problem = await Database.selectOneFrom("problems", "*", {
-            id: problemId,
-        });
+    memoizedRequestExtractor(
+        req,
+        "__problem_" + problemId,
+        async (): Promise<
+            | Problem
+            | Omit<
+                  Problem,
+                  | "evaluation_language"
+                  | "evaluation_script"
+                  | "solution_language"
+                  | "solution_code"
+              >
+        > => {
+            const problem = await Database.selectOneFrom("problems", "*", {
+                id: problemId,
+            });
 
-        if (!problem) throw new SafeError(StatusCodes.NOT_FOUND);
+            if (!problem) throw new SafeError(StatusCodes.NOT_FOUND);
 
-        const contest = await extractContest(req, problem.contest_id);
+            const contest = await extractContest(req, problem.contest_id);
 
-        // TODO: Find a better way of doing this
-        const optionalUser = await extractOptionalUser(req);
+            // TODO: Find a better way of doing this
+            const optionalUser = await extractOptionalUser(req);
 
-        const optionalMember =
-            optionalUser &&
-            (await Database.selectOneFrom("contest_members", "*", {
-                contest_id: contest.id,
-                user_id: optionalUser.id,
-            }));
+            const optionalMember =
+                optionalUser &&
+                (await Database.selectOneFrom("contest_members", "*", {
+                    contest_id: contest.id,
+                    user_id: optionalUser.id,
+                }));
 
-        if (
-            Date.now() >= contest.start_time.getTime() &&
-            (!optionalMember ||
-                !hasContestPermission(
-                    optionalMember.contest_permissions,
+            if (
+                Date.now() >= contest.start_time.getTime() &&
+                (!optionalMember ||
+                    !hasContestPermission(
+                        optionalMember.contest_permissions,
+                        ContestMemberPermissions.VIEW_PRIVATE
+                    )) &&
+                (!optionalUser ||
+                    !hasAdminPermission(optionalUser.permissions, AdminPermissions.VIEW_CONTEST))
+            )
+                return R.omit(problem, [
+                    "evaluation_language",
+                    "evaluation_script",
+                    "solution_language",
+                    "solution_code",
+                ]);
+
+            const user = await extractUser(req);
+
+            if (hasAdminPermission(user.permissions, AdminPermissions.VIEW_CONTEST)) return problem;
+
+            const member = await extractContestMember(req, problem.contest_id);
+
+            if (
+                hasContestPermission(
+                    member.contest_permissions,
                     ContestMemberPermissions.VIEW_PRIVATE
-                )) &&
-            (!optionalUser ||
-                !hasAdminPermission(optionalUser.permissions, AdminPermissions.VIEW_CONTEST))
-        )
-            return R.omit(problem, ["evaluation_script", "solution_language", "solution_code"]);
+                )
+            )
+                return problem;
 
-        const user = await extractUser(req);
-
-        if (hasAdminPermission(user.permissions, AdminPermissions.VIEW_CONTEST)) return problem;
-
-        const member = await extractContestMember(req, problem.contest_id);
-
-        if (hasContestPermission(member.contest_permissions, ContestMemberPermissions.VIEW_PRIVATE))
-            return problem;
-
-        throw new SafeError(StatusCodes.NOT_FOUND);
-    });
+            throw new SafeError(StatusCodes.NOT_FOUND);
+        }
+    );
