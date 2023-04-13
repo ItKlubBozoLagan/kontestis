2d81f7fb522df6a986251dc5c362ee7f809aa2fc
VisenP <visen.pavlica12@gmail.com>
2023-04-13 22:45:17 +0200

    Redirect admin manage contests to management page

--- apps/frontend/src/pages/admin/contests/AdminContestsPage.tsx
+++ apps/frontend/src/pages/admin/contests/AdminContestsPage.tsx
@@ -1,5 +1,15 @@
 import { FC } from "react";
+import { useNavigate } from "react-router";
+
+import { SimpleButton } from "../../../components/SimpleButton";
 
 export const AdminContestsPage: FC = () => {
-    return <div></div>;
+    const navigate = useNavigate();
+
+    return (
+        <div tw={"w-full flex flex-col items-center gap-4"}>
+            <span tw={"text-lg"}>Admin can manage contests in the management page</span>
+            <SimpleButton onClick={() => navigate("/management")}>Go to management</SimpleButton>
+        </div>
+    );
 };
