2ceeefffadfd291037b7385a31772fcd10d964ff
Antony1060 <antoniostignjedec@gmail.com>
2023-04-13 21:48:10 +0200

    Better notifications dropdown

--- apps/frontend/src/components/NotificationBellDropdown.tsx
+++ apps/frontend/src/components/NotificationBellDropdown.tsx
@@ -2,7 +2,15 @@ import { SiteNotification, SiteNotificationType } from "@kontestis/models";
 import { toCroatianLocale } from "@kontestis/utils";
 import { FC, useEffect, useMemo, useState } from "react";
 import { IconType } from "react-icons";
-import { FiAlertCircle, FiAlertTriangle, FiBell, FiClock, FiHelpCircle } from "react-icons/all";
+import {
+    AiFillCaretDown,
+    AiFillCaretUp,
+    FiAlertCircle,
+    FiAlertTriangle,
+    FiBell,
+    FiClock,
+    FiHelpCircle,
+} from "react-icons/all";
 import tw from "twin.macro";
 
 import { useReadNotifications } from "../hooks/notifications/useReadNotifications";
@@ -24,6 +32,7 @@ const NotificationTypeIconMap: Record<SiteNotificationType, IconType> = {
 
 export const NotificationBellDropdown: FC<Properties> = ({ notifications }) => {
     const [expanded, setExpanded] = useState(false);
+    const [listExpanded, setListExpanded] = useState(false);
     const [readTimeout, setReadTimeout] = useState<ReturnType<typeof setTimeout>>();
 
     const newNotificationIds = useMemo(
@@ -62,7 +71,7 @@ export const NotificationBellDropdown: FC<Properties> = ({ notifications }) => {
         <div>
             <div tw={"flex items-center text-neutral-600 cursor-pointer relative"}>
                 <FiBell size={"18px"} onClick={() => setExpanded((previous) => !previous)} />
-                {newNotificationIds.length > 0 && !isLoading && (
+                {newNotificationIds.length > 0 && (
                     <div tw={"absolute -top-0.5 right-0.5 w-2 h-2 rounded-full bg-red-400"}></div>
                 )}
             </div>
@@ -78,33 +87,61 @@ export const NotificationBellDropdown: FC<Properties> = ({ notifications }) => {
                     {notifications.length === 0 ? (
                         <span tw={"p-4 text-center"}>You&apos;re up to date!</span>
                     ) : (
-                        sortedNotifications.map(
-                            (it, index, _, Icon = NotificationTypeIconMap[it.type]) => (
-                                <div
-                                    key={it.id.toString()}
-                                    tw={"py-2 px-3 flex flex-col gap-1"}
-                                    css={index % 2 === 0 ? tw`bg-neutral-100` : tw`bg-neutral-200`}
-                                >
+                        <>
+                            {sortedNotifications
+                                .slice(0, listExpanded ? sortedNotifications.length : 4)
+                                .map((it, index, _, Icon = NotificationTypeIconMap[it.type]) => (
                                     <div
-                                        tw={
-                                            "w-full text-right text-neutral-600 text-sm flex items-center gap-2 select-none"
-                                        }
+                                        key={it.id.toString()}
+                                        tw={"py-2 px-3 flex flex-col gap-1"}
                                         css={
-                                            newNotificationIds.includes(it.id) ? tw`font-bold` : ""
+                                            index % 2 === 0
+                                                ? tw`bg-neutral-100`
+                                                : tw`bg-neutral-200`
                                         }
                                     >
-                                        <Icon size={"16px"} />
-                                        {toCroatianLocale(it.created_at)}
+                                        <div
+                                            tw={
+                                                "w-full text-right text-neutral-600 text-sm flex items-center gap-2 select-none"
+                                            }
+                                            css={
+                                                newNotificationIds.includes(it.id)
+                                                    ? tw`font-bold`
+                                                    : ""
+                                            }
+                                        >
+                                            <Icon size={"16px"} />
+                                            {toCroatianLocale(it.created_at)}
+                                        </div>
+                                        <div>
+                                            {newNotificationIds.includes(it.id) && <span>* </span>}
+                                            <Translated translationKey={`notifications.${it.type}`}>
+                                                <span tw={"font-bold"}>{it.data}</span>
+                                            </Translated>
+                                        </div>
                                     </div>
-                                    <div>
-                                        {newNotificationIds.includes(it.id) && <span>* </span>}
-                                        <Translated translationKey={`notifications.${it.type}`}>
-                                            <span tw={"font-bold"}>{it.data}</span>
-                                        </Translated>
-                                    </div>
+                                ))}
+                            {sortedNotifications.length > 4 && (
+                                <div
+                                    tw={
+                                        "w-full p-2 flex justify-center items-center gap-2 cursor-pointer bg-neutral-300 bg-opacity-80"
+                                    }
+                                    onClick={() => setListExpanded((previous) => !previous)}
+                                >
+                                    {listExpanded ? (
+                                        <>
+                                            <AiFillCaretUp />
+                                            Collapse
+                                        </>
+                                    ) : (
+                                        <>
+                                            <AiFillCaretDown />
+                                            Expand
+                                        </>
+                                    )}
                                 </div>
-                            )
-                        )
+                            )}
+                        </>
                     )}
                 </div>
             )}
