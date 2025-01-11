d2b1d7533dd2d236b6c096bdd170ebcb6acf16a3
Antonio F. Trstenjak <antoniostignjedec@gmail.com>
2025-01-11 21:53:51 +0100

    Comment out managed login UI

--- apps/frontend/src/pages/auth/LoginPage.tsx
+++ apps/frontend/src/pages/auth/LoginPage.tsx
@@ -6,7 +6,6 @@ import { AaiEduButton } from "../../components/AaiEduButton";
 import { TitledSection } from "../../components/TitledSection";
 import { useTranslation } from "../../hooks/useTranslation";
 import { useTokenStore } from "../../state/token";
-import { ManagedLoginForm } from "./ManagedLoginForm";
 
 const LoginBase: FC = () => {
     const { setToken } = useTokenStore();
@@ -27,34 +26,32 @@ const LoginBase: FC = () => {
         <div tw={"w-full md:max-w-[768px] mt-20"}>
             <TitledSection title={t("login.label")}>
                 <div tw={"flex gap-4 justify-center items-stretch w-full py-12"}>
-                    <div tw={"flex flex-col gap-4 items-center w-full px-12 py-4"}>
-                        <span tw={"text-lg"}>Log in with email</span>
-                        <div tw={"w-full flex flex-col gap-4 items-center flex-grow"}>
-                            <ManagedLoginForm />
-                        </div>
+                    {/*<div tw={"flex flex-col gap-4 items-center w-full px-12 py-4"}>*/}
+                    {/*    <span tw={"text-lg"}>Log in with email</span>*/}
+                    {/*    <div tw={"w-full flex flex-col gap-4 items-center flex-grow"}>*/}
+                    {/*        <ManagedLoginForm />*/}
+                    {/*    </div>*/}
+                    {/*</div>*/}
+                    {/*<div tw={"flex-shrink-0 flex flex-col items-center justify-center gap-1"}>*/}
+                    {/*    <div tw={"w-[1px] flex-grow bg-neutral-400"}></div>*/}
+                    {/*    <span tw={"text-neutral-800 mb-1"}>or</span>*/}
+                    {/*    <div tw={"w-[1px] flex-grow bg-neutral-400"}></div>*/}
+                    {/*</div>*/}
+                    {/*<div tw={"flex flex-col gap-6 items-center w-full py-4"}>*/}
+                    {/*    <span tw={"text-lg"}>Log in with SSO</span>*/}
+                    <div tw={"flex flex-col flex-grow gap-6 items-center justify-center w-full"}>
+                        <GoogleLogin
+                            onSuccess={onGoogleLoginSuccess}
+                            width={"256px"}
+                            size={"large"}
+                            text={"signin"}
+                            theme={"outline"}
+                            shape={"rectangular"}
+                            auto_select={true}
+                        />
+                        <AaiEduButton purpose={"login"} />
                     </div>
-                    <div tw={"flex-shrink-0 flex flex-col items-center justify-center gap-1"}>
-                        <div tw={"w-[1px] flex-grow bg-neutral-400"}></div>
-                        <span tw={"text-neutral-800 mb-1"}>or</span>
-                        <div tw={"w-[1px] flex-grow bg-neutral-400"}></div>
-                    </div>
-                    <div tw={"flex flex-col gap-6 items-center w-full py-4"}>
-                        <span tw={"text-lg"}>Log in with SSO</span>
-                        <div
-                            tw={"flex flex-col flex-grow gap-6 items-center justify-center w-full"}
-                        >
-                            <GoogleLogin
-                                onSuccess={onGoogleLoginSuccess}
-                                width={"256px"}
-                                size={"large"}
-                                text={"signin"}
-                                theme={"outline"}
-                                shape={"rectangular"}
-                                auto_select={true}
-                            />
-                            <AaiEduButton purpose={"login"} />
-                        </div>
-                    </div>
+                    {/*</div>*/}
                 </div>
             </TitledSection>
         </div>
