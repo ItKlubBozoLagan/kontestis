9b3b1bce882928eb1574a1f9b185c36ddadc26d2
Antonio F. Trstenjak <antoniostignjedec@gmail.com>
2025-02-11 00:54:43 +0100

    Introduce react compiler

--- apps/frontend-new/vite.config.ts
+++ apps/frontend-new/vite.config.ts
@@ -3,9 +3,17 @@ import * as path from "node:path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+const ReactCompilerConfig = {};
+
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react({
+            babel: {
+                plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
+            },
+        }),
+    ],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "src"),
