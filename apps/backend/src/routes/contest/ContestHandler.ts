fb4fb899f0ea46836610627b9ec4db4f067b5a01
Antonio F. T. <antoniostignjedec@gmail.com>
2025-10-21 10:07:53 +0200

    Use edu user name on leaderboard if present (#114)
    * Use edu user name on leaderboard if present
    
    * Apply name change only on edu contests

--- apps/backend/src/routes/contest/ContestHandler.ts
+++ apps/backend/src/routes/contest/ContestHandler.ts
@@ -490,11 +490,12 @@ ContestHandler.get("/:contest_id/leaderboard", async (req, res) => {
                     eduUser = eduUsers.find((user) => user.id === it.user_id)
                 ) => ({
                     ...it,
-                    ...R.pick(user, ["full_name"]),
                     ...R.pick(
                         organisationMembers.find((member) => member.user_id === it.user_id)!,
                         ["elo"]
                     ),
+                    full_name:
+                        (contest.require_edu_verification && eduUser?.full_name) || user.full_name,
                     email_domain: user.email.split("@").at(-1),
                     edu_mail_domain: eduUser?.email.split("@").at(-1),
                 })
